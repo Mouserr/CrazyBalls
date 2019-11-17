5f633ec877df95d946906857dee6182aef7adc42
Lost_Daemon <lost_daemon@homail.com>
2019-11-17 19:11:39 +0300

    Battle group and Team

---
+++ Assets/Scripts/Teams/AllyTeam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Scripts.Core;
+
+namespace Assets.Scripts
+{
+    public sealed class AllyTeam 
+    {
+        public List<AllyCharacter> Members = new List<AllyCharacter>();
+    }
+}
---
+++ Assets/Scripts/Teams/BattleGroup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Teams
+{
+    public sealed class BattleGroup
+    {
+        private readonly int _groupSize;
+        private readonly Character[] _members;
+
+        public IEnumerable<Character> Members => _members.ToList();
+
+        public BattleGroup(int groupSize = 3)
+        {
+            _groupSize = groupSize;
+            _members = new Character[_groupSize];
+        }
+
+        public void SetMember(int position, Character member)
+        {
+            if(position<0 || position>=_groupSize) return;
+            _members[position] = member;
+        }
+    }
+}
